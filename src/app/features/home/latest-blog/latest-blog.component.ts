6840341f884aef6c48bc4c50428a3834462b2495
danieljancar <danieljancar@bluewin.ch>
2023-12-18 15:07:08 +0100

    feat: component creation

---
+++ src/app/features/home/latest-blog/latest-blog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-blog',
+  standalone: true,
+  imports: [],
+  templateUrl: './latest-blog.component.html',
+  styleUrl: './latest-blog.component.scss'
+})
+export class LatestBlogComponent {
+
+}
---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import {DateUtil} from "../../utils/date.util";
+import {NgIcon, provideIcons} from "@ng-icons/core";
+import {bootstrapGithub, bootstrapLinkedin, bootstrapStackOverflow} from "@ng-icons/bootstrap-icons";
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    NgIcon
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  viewProviders: [
+    provideIcons({
+      bootstrapGithub,
+      bootstrapLinkedin,
+      bootstrapStackOverflow,
+    }),
+  ],
+})
+export class FooterComponent {
+  public readonly currentYear: string = DateUtil.formatDate(new Date(), 'yyyy');
+}
