2dbddc46363c0a5696079172c1e7ea4197ff85cb
Daniel Jancar <danieljancar@bluewin.ch>
2024-01-02 14:36:19 +0100

    fix: add missing icons to blog detail

--- src/app/features/blog-detail/blog-detail.component.ts
+++ src/app/features/blog-detail/blog-detail.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-import { NgIcon } from '@ng-icons/core';
+import {Component} from '@angular/core';
+import {NgIcon, provideIcons} from '@ng-icons/core';
+import {bootstrapConeStriped} from "@ng-icons/bootstrap-icons";
 
 @Component({
   selector: 'app-blog-detail',
   standalone: true,
   imports: [NgIcon],
+  viewProviders: [
+    provideIcons({bootstrapConeStriped})
+  ],
   templateUrl: './blog-detail.component.html',
   styleUrl: './blog-detail.component.scss',
 })
-export class BlogDetailComponent {}
+export class BlogDetailComponent {
+}
