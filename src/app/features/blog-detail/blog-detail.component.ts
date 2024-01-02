a9b708c0bd4573bfdbeda56f7aded9c3e6c4bb88
danieljancar <danieljancar@users.noreply.github.com>
2024-01-02 13:37:15 +0000

    Prettified Code!

--- src/app/features/blog-detail/blog-detail.component.ts
+++ src/app/features/blog-detail/blog-detail.component.ts
@@ -1,16 +1,13 @@
-import {Component} from '@angular/core';
-import {NgIcon, provideIcons} from '@ng-icons/core';
-import {bootstrapConeStriped} from "@ng-icons/bootstrap-icons";
+import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { bootstrapConeStriped } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-blog-detail',
   standalone: true,
   imports: [NgIcon],
-  viewProviders: [
-    provideIcons({bootstrapConeStriped})
-  ],
+  viewProviders: [provideIcons({ bootstrapConeStriped })],
   templateUrl: './blog-detail.component.html',
   styleUrl: './blog-detail.component.scss',
 })
-export class BlogDetailComponent {
-}
+export class BlogDetailComponent {}
