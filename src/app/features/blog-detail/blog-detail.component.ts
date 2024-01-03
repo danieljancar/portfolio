5ba1088d2398a19783179c735d8f60603be6f98b
danieljancar <danieljancar@bluewin.ch>
2024-01-03 15:11:06 +0100

    feat: Add marked and ngx-markdown to portfolio dependencies
    Marked and ngx-markdown libraries have been added to the package.json and package-lock.json files in the portfolio project. These additions will provide markdown support within the portfolio.

--- src/app/features/blog-detail/blog-detail.component.ts
+++ src/app/features/blog-detail/blog-detail.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Blog, BlogsJson } from '../../types/blog.type';
 import * as blogData from '../../content/blog/blog.json';
+import { MarkdownComponent, provideMarkdown } from 'ngx-markdown';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-blog-detail',
   templateUrl: './blog-detail.component.html',
   styleUrls: ['./blog-detail.component.scss'],
+  providers: [provideMarkdown({ loader: HttpClient })],
   standalone: true,
+  imports: [MarkdownComponent],
 })
 export class BlogDetailComponent implements OnInit {
   blogData: Blog | undefined;
