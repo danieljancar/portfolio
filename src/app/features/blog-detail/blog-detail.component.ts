bac7e700809b49d1c2675e32a82a0669644b88e7
danieljancar <danieljancar@bluewin.ch>
2024-01-04 11:08:27 +0100

    fix: refactor blog structure and add new introduction component
    The commit introduces a new introduction component and modifies corresponding blog details. This includes loading blog data, and enhances the process with the addition of author handling logic. The file structure of blog assets has been updated for better organization, moving some content files to a separate assets directory. The changes also include minor updates in blog data, adding read time and adjusting some formatting.

--- src/app/features/blog-detail/blog-detail.component.ts
+++ src/app/features/blog-detail/blog-detail.component.ts
@@ -1,34 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Blog, BlogsJson } from '../../types/blog.type';
-import * as blogData from '../../content/blog/blog.json';
 import { MarkdownComponent, provideMarkdown } from 'ngx-markdown';
 import { HttpClient } from '@angular/common/http';
-import { NgOptimizedImage } from '@angular/common';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import {
-  bootstrapBoxArrowUpRight,
-  bootstrapRobot,
-} from '@ng-icons/bootstrap-icons';
+import { DatePipe, NgOptimizedImage } from '@angular/common';
+import { NgIcon } from '@ng-icons/core';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { Blog, BlogsJson } from '../../types/blog.type';
+import * as blogData from '../../content/blog.json';
+import { Author } from '../../types/author.type';
 
 @Component({
   selector: 'app-blog-detail',
   templateUrl: './blog-detail.component.html',
   styleUrls: ['./blog-detail.component.scss'],
   providers: [provideMarkdown({ loader: HttpClient })],
-  viewProviders: [
-    provideIcons({
-      bootstrapBoxArrowUpRight,
-      bootstrapRobot,
-    }),
-  ],
   standalone: true,
-  imports: [MarkdownComponent, NgOptimizedImage, NgIcon],
+  imports: [
+    MarkdownComponent,
+    NgOptimizedImage,
+    NgIcon,
+    DatePipe,
+    IntroductionComponent,
+  ],
 })
 export class BlogDetailComponent implements OnInit {
-  blogData: Blog | undefined;
+  public blog: Blog | undefined;
+  public authorName: string | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {
+    window.scrollTo(0, 0);
+  }
 
   ngOnInit(): void {
     const blogId = this.route.snapshot.paramMap.get('blogId');
@@ -39,6 +40,18 @@ export class BlogDetailComponent implements OnInit {
 
   private loadBlogData(blogId: string): void {
     const blogs: Blog[] = (blogData as BlogsJson).blogs;
-    this.blogData = blogs.find(blog => blog.slug === blogId);
+    this.blog = blogs.find(blog => blog.slug === blogId);
+    if (this.blog) {
+      this.getAuthor(this.blog.author);
+    }
+  }
+
+  public async getAuthor(username: string | undefined) {
+    const authors: Author[] = (blogData as BlogsJson).authors;
+    const author = authors.find(author => author.username === username);
+    if (author) {
+      this.authorName = author.name;
+    }
+    return author;
   }
 }
